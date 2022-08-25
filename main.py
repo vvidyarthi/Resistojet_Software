58277fe73f9e70979af14fafebaf3ebdfeb99b6c
vvidyarthi <vvidyarthi@benchmark-space.com>
2022-08-25 16:10:08 -0400

    Signal management update

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from email.errors import ObsoleteHeaderDefect
 from os import stat
 import time
 import sys
@@ -6,19 +7,32 @@ import qtconfig
 from PySide6.QtWidgets import (
     QApplication,
     QMainWindow)
-from PySide6.QtCore import QThread, QTimer
+from PySide6.QtCore import QThread, pyqtSignal, QTimer
 
 
 class Worker(QThread):
+    
+    time_signal = pyqtSignal(object)
+    tc1_signal = pyqtSignal(object)
+    tc2_signal = pyqtSignal(object)
+    flow_signal = pyqtSignal(object)
+    voltage_signal = pyqtSignal(object)
+    power_signal = pyqtSignal(object)
 
     def __init__(self):
         self.init_time = time.time()
         QThread.__init__(self)
     
-    def run(self):
+    def run(self):  
         while True:
-            
-            collector_.collect_data(self.init_time)
+            self.data_array = collector_.collect_data(self.init_time)
+            self.time_signal.emit(collector_.time_array)
+            self.tc1_signal.emit(collector_.tc1_array)
+            self.tc2_signal.emit(collector_.tc2_array)
+            self.flow_signal.emit(collector_.flow_array)
+            self.voltage_signal.emit(collector_.voltage_array)
+            self.power_signal.emit(collector_.power_array)
+
         # plotter_.plot_data(collector_.time_array, collector_.tc1_array, collector_.tc2_array, 
         #     collector_.flow_array, collector_.voltage_array, collector_.power_array)
 
@@ -40,18 +54,29 @@ class MainWindow(QMainWindow):
     def __init__(self, widget):
         QMainWindow.__init__(self)
         self.worker = None
-        self.background_task()
+        self.data_collecting_worker()
         self.setWindowTitle("Benchmark Space Systems Resistojet Software")
         self.setCentralWidget(widget)
 
-    def background_task(self):
+    def data_collecting_worker(self):
         self.worker = Worker()
-        self.worker.started.connect(self.worker.run)
+        self.worker.time_signal.connect(self.display)
+        self.worker.tc1_signal.connect(self.display)
+        self.worker.tc2_signal.connect(self.display)
+        self.worker.flow_signal.connect(self.display)
+        self.worker.voltage_signal.connect(self.display)
+        self.worker.power_signal.connect(self.display)
         self.worker.start()
+        # self.worker.started.connect(self.worker.run)
+        # self.worker.start()
         # self.update_timer = QTimer()
-        # self.update_timer.setInterval(100)
+        # self.update_timer.setInterval(10)
         # self.update_timer.timeout.connect(self.worker.run)
         # self.update_timer.start()
+    def dispay(self):
+        plotter_.plot_data(collector_.time_array, collector_.tc1_array, collector_.tc2_array, 
+            collector_.flow_array, collector_.voltage_array, collector_.power_array)
+
 
 
 if __name__ == "__main__":
