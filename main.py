2392d9e9ce98305e3d9a42c3aa2f3816aa5610ba
vvidyarthi <vvidyarthi@benchmark-space.com>
2022-09-06 11:11:35 -0400

    Reverting to worker control

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ class Worker(QObject):
         while True:
             time.sleep(0.1)
 
+            collector_.collect_data(self.init_time)
             if state_.control_state == 0:
                 pass
 
@@ -29,16 +30,8 @@ class Worker(QObject):
                 controller_.control_lifetime(state_.cycle_number, state_.voltage, state_.high_temp, state_.low_temp, state_.high_time, state_.low_time, collector_.tc1_array)
                 widget_.logger.log_data(collector_.data_array)
 
-class DataWorker(QObject):
-    def __init__(self):
-        self.init_time = time.time()
-        QObject.__init__(self)
-
-    def run(self):
-        time.sleep(0.1)
-        collector_.collect_data(self.init_time)
-        print(f'tc1: {collector_.tc1:<10.3f}  tc2: {collector_.tc2:<10.3f}  tc3: {collector_.tc3:<10.3f}   tc4: {collector_.tc4:<10.3f}')
-        plotter_.plot_data(collector_.time_array, collector_.tc1_array, collector_.tc2_array, collector_.flow_array, collector_.voltage_array, collector_.power_array)
+            print(f'tc1: {collector_.tc1:<10.3f}  tc2: {collector_.tc2:<10.3f}  tc3: {collector_.tc3:<10.3f}   tc4: {collector_.tc4:<10.3f}')
+            plotter_.plot_data(collector_.time_array, collector_.tc1_array, collector_.tc2_array, collector_.flow_array, collector_.voltage_array, collector_.power_array)
 
 class MainWindow(QMainWindow):
 
@@ -49,7 +42,6 @@ class MainWindow(QMainWindow):
         self.thread = None
         self.data_thread = None
 
-        self.data_task()
         self.worker_task()
         self.setWindowTitle("Benchmark Space Systems Resistojet Software")
         self.setCentralWidget(widget)
@@ -60,13 +52,6 @@ class MainWindow(QMainWindow):
         self.worker.moveToThread(self.thread)
         self.thread.started.connect(self.worker.run)
         self.thread.start()
-
-    def data_task(self):
-        self.data_thread = QThread()
-        self.data_worker = DataWorker()
-        self.data_worker.moveToThread(self.data_thread)
-        self.data_thread.started.connect(self.data_worker)
-        self.data_thread.start()
 
 
 if __name__ == "__main__":
